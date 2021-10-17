f24f33fcef98d1d204ef78eef5d9766f140c4bc1
Pierre-Yves Fourmond <pyfourmond@gmail.com>
2021-10-17 07:14:05 +0200

    Add failing unit test

--- src/main/resources/archetype-resources/src/test/java/ApplicationTest.java
+++ src/main/resources/archetype-resources/src/test/java/ApplicationTest.java
@@ -1,7 +1,13 @@
 package ${package};
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class ApplicationTest
 {
+    @Test
+    public void test()
+    {
+        fail("Please implement a test here !");
+    }
 }
