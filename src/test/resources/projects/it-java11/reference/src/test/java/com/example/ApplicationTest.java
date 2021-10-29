60f9a73475222fb26486213b476b339053167c2c
Pierre-Yves Fourmond <pyfourmond@gmail.com>
2021-10-29 07:15:22 +0200

    Add reference folder for integration tests generated project

---
+++ src/test/resources/projects/it-java11/reference/src/test/java/com/example/ApplicationTest.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ApplicationTest
+{
+    @Test
+    public void shouldSucceed()
+    {
+    }
+
+    @Test
+    public void shouldFail()
+    {
+        fail("Please implement a test here !");
+    }
+}
